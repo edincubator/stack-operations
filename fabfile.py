bf54a2e245bce72b2486cdaee0492f4b823cd389
Mikel Emaldi <m.emaldi@gmail.com>
2018-06-07 11:44:10 +0200

    creating kafka topics

--- fabfile.py
+++ fabfile.py
@@ -353,3 +353,28 @@ def delete_hbase_namespace(namespace_name):
     run('kinit -kt /etc/security/keytabs/hbase.headless.keytab {}'.format(
         settings.hbase_principal))
     run('echo "drop_namespace \'{}\'" | hbase shell'.format(namespace_name))
+
+
+@task
+@roles('master')
+def create_kafka_topic(username, topic, partitions=1, replication_factor=1):
+    run('kinit -kt /etc/security/keytabs/hdfs.headless.keytab {}'.format(
+        settings.hdfs_principal))
+
+    run('{client_dir}/bin/kafka-topics.sh --create --zookeeper '
+        '{zookeeper_servers} --topic {topic} --partitions {partitions} '
+        '--replication-factor {replication_factor}'.format(
+            client_dir=settings.kafka_client_dir,
+            zookeeper_servers=' '.join(settings.zookeeper_servers),
+            topic=topic,
+            partitions=partitions,
+            replication_factor=replication_factor
+        ))
+
+    execute(create_ranger_policy,
+            topic,
+            username,
+            'kafka_{}'.format(topic),
+            'kafka policy for topic {}'.format(topic),
+            'topic',
+            'kafka')
