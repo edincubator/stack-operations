7119c6323e9b655e9f1eb338ca71bea4afe4aa65
Mikel Emaldi <m.emaldi@gmail.com>
2018-07-02 09:25:01 +0200

    Adapting code for using an external config file

--- operations/user.py
+++ operations/user.py
@@ -7,17 +7,16 @@ import hdfs
 import kerberos
 import ldap
 import ranger
-import settings
 import unix
 from fabric.connection import Connection
 from invoke import run, task
 
 user = 'root'
 
-ldap_connection = Connection(settings.ldap_host, user)
-kerberos_connection = Connection(settings.kerberos_host, user)
-ranger_connection = Connection(settings.ranger_host, user)
-master_connection = Connection(settings.master_host, user)
+# ldap_connection = Connection(settings.ldap_host, user)
+# kerberos_connection = Connection(settings.kerberos_host, user)
+# ranger_connection = Connection(settings.ranger_host, user)
+# master_connection = Connection(settings.master_host, user)
 
 
 @task(help={'username': 'Username of the user to be created',
@@ -29,21 +28,24 @@ def new(c, username, mail, group=None):
     """
     password = os.urandom(6).encode('hex')
 
-    for host in settings.hosts:
-        c = Connection(host, user)
-        unix.create_unix_user(c, username)
+    for host in c.config.hosts:
+        connection = Connection(host, user)
+        unix.create_unix_user(connection, username)
 
+    ldap_connection = Connection(c.config.ldap_host, user)
     ldap.create_ldap_user(ldap_connection, username, password)
-
     if group is not None:
         ldap.add_user_to_ldap_group(ldap_connection, username, group)
 
+    kerberos_connection = Connection(c.config.kerberos_host, user)
     kerberos.create_kerberos_user(kerberos_connection, username, password)
     folder_uuid, file_uuid = kerberos.create_nifi_keytab(
         kerberos_connection, username)
 
+    ranger_connection = Connection(c.config.ranger_host, user)
     ranger.update_ranger_usersync(ranger_connection)
 
+    master_connection = Connection(c.config.master_host, user)
     hdfs.create_hdfs_home(master_connection, username)
     ranger.create_ranger_policy(
             ['/user/{}'.format(username)],
@@ -56,7 +58,7 @@ def new(c, username, mail, group=None):
     ranger.update_nifi_flow_ranger_policy(username)
 
     sync_ambari_users()
-    send_password_mail(username, password, mail, folder_uuid, file_uuid)
+    send_password_mail(c, username, password, mail, folder_uuid, file_uuid)
 
 
 @task(help={'username': 'Username of the user to be deleted'})
@@ -64,14 +66,15 @@ def delete(c, username):
     """
     Deletes a user from the system.
     """
+    ldap_connection = Connection(c.config.ldap_host, user)
     ldap.delete_ldap_user(ldap_connection, username)
-    for host in settings.hosts:
-        c = Connection(host, user)
-        unix.delete_unix_user(c, username)
-    sync_ambari_users()
+    for host in c.config.hosts:
+        connection = Connection(host, user)
+        unix.delete_unix_user(connection, username)
+    sync_ambari_users(c)
 
 
-def sync_ambari_users():
+def sync_ambari_users(c):
     content = [
         {"Event":
             {"specs":
@@ -83,12 +86,12 @@ def sync_ambari_users():
     run("curl -u admin -H 'X-Requested-By: ambari' -X POST -d "
         "'{content}' {ambari_url}/api/v1/ldap_sync_events".format(
             content=json.dumps(content),
-            ambari_url=settings.ambari_url), pty=True)
+            ambari_url=c.config.ambari_url), pty=True)
 
 
-def send_password_mail(username, password, mail, folder_uuid, file_uuid):
+def send_password_mail(c, username, password, mail, folder_uuid, file_uuid):
     principal = '{username}@{realm}'.format(username=username,
-                                            realm=settings.kerberos_realm)
+                                            realm=c.config.kerberos_realm)
     msg = MIMEText('''Welcome to EDI Big Data Stack!\n\n
 Those are your credentials for accesing to the system:\n\n
 LDAP username: {username}\n
@@ -104,11 +107,11 @@ NiFi keytab: {keytab}'''.format(
         )))
     msg['Subject'] = "[European Data Incubator] Big Data Stack Credentials"
     msg['From'] = "EDI - European Data Incubator <{}>".format(
-        settings.mail_from)
+        c.config.mail_from)
     msg['To'] = mail
 
-    server = smtplib.SMTP(settings.mail_host, settings.mail_port)
-    if settings.mail_user is not None:
-        server.login(settings.mail_user, settings.mail_password)
-    server.sendmail(settings.mail_from, [mail], msg.as_string())
+    server = smtplib.SMTP(c.config.mail_host, c.config.mail_port)
+    if c.config.mail_user is not None:
+        server.login(c.config.mail_user, c.config.mail_password)
+    server.sendmail(c.config.mail_from, [mail], msg.as_string())
     server.quit()
